4f53eea775c693db9e4637a3f662b9aeb77ec6f4
Joon Park <joonpark@Joons-MacBook-Pro.local>
2016-02-21 18:19:07 -0500

    bug fix

--- chapter6/public/notesApp.js
+++ chapter6/public/notesApp.js
@@ -2,9 +2,21 @@ angular.module('notesApp', [])
   .controller('MainCtrl', ['$http', '$log', function($http, $log){
     var self = this;
     self.items = [];
-    $http.get('/api/note').then(function(successResponse){
-      self.items = successReponse.data;
+
+    var fetchTodos = function(){
+      return $http.get('/api/note').then(function(successResponse){
+      self.items = successResponse.data;
     }, function(failResponse){
       $log.error("failed response status" + failResponse.status);
-    });
+    })};
+
+    fetchTodos();
+
+    self.add = function(){
+      $http.post('/api/note', self.newTodo)
+      .then(fetchTodos)
+      .then(function(response){
+        self.newTodo = {};
+      })
+    }
   }]);
